c6c8d8fc34063e96dffb6c4818812b41302e41e9
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-03-15 17:00:10 -0300

    feat: add req07 - create validations for .post /users

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import userController from '../controllers/user.controller';
+import validateUser from '../middlewares/validateUser';
+
+const { validateUsername, validateVocation, validateLevel, validatePassword } = validateUser;
 
 const router = Router();
 
-router.post('/', userController.create);
+router
+  .post('/', validateUsername, validateVocation, validateLevel, validatePassword, userController
+    .create);
 
 export default router;
